e7309b018e3dcd5a8c12b4b0467590007f9be80a
cjingguo <1311038265@qq.com>
2022-04-01 13:01:11 +0800

    构建项目、配置全局样式、路由、elemengui

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'; /*引入*/
+Vue.use(ElementUI); /*注入*/
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
---
+++ src/views/Login/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="login">登录页</div>
+</template>
+<script>
+export default{
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh; /*可视区100%高度*/
+    background-color: #344a5f;
+}
+</style>
